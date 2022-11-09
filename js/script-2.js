485b106b9d508546863c71e5416936579f57b932
Ev-jan <evgen.mazyar@gmail.com>
2022-11-09 12:33:54 +0400

    bug fixed in loop

--- js/script-2.js
+++ js/script-2.js
@@ -1,46 +1,49 @@
 const slides = [
-    {   caption: 'Rostov-on-Don, Admiral',
-        city: 'Rostov-on-Don LCD admiral',
+    {   city: 'Rostov-on-Don LCD admiral',
         area: '81 m2',
         time: '3.5 months',
-        img: "/images-content/img-slide-1.png"
+        image: "/images-content/img-slide-1.png"
     },
-    {   caption: 'Sochi Thieves',
-        city: 'Sochi Thieves',
+    {   city: 'Sochi Thieves',
         area: '105 m2',
         time: '4 months',
-        img: "/images-content/img-slide-2.png"
+        image: "/images-content/img-slide-2.png"
     },
-    {   caption: 'Rostov-on-Don Patriotic',
-        city: 'Rostov-on-Don Patriotic',
+    {   city: 'Rostov-on-Don Patriotic',
         area: '93 m2',
         time: '3 months',
-        img: "/images-content/img-slide-3.png"
-    },
+        image: "/images-content/img-slide-3.png"
+    }
   ]
   
-  let caption = document.getElementsByClassName("caption-text");
-  let city = document.getElementsByClassName("info-block__text_city");
-  let area = document.getElementsByClassName("info-block__text_area");
-  let time = document.getElementsByClassName("info-block__text_time");
+  let city = document.querySelector(".info-block__text_city");
+  let area = document.querySelector(".info-block__text_area");
+  let time = document.querySelector(".info-block__text_time");
+  let img = document.querySelector(".slideshow-container");
+  let dots = document.getElementsByClassName("dot-indicator");
+  let captions = document.getElementsByClassName("caption-text");
+  let slideIndex = 1;
   
-  let setSlide = (index) => {
-    caption.innerText = slides[index].caption;
-    img.style.backgroundImage = `url(${slides[index].img})`;
-    city.innerText = slides[index].city;
-    area.innerText = slides[index].area;
-    time.innerText = slides[index].time;
-  }
-  
-  const prev = document.querySelector('.prev');
-  const next = document.querySelector('.next');
-  let currentIndex = 0;
-  
-  prev.addEventListener('click', () => {
-    setSlide(currentIndex - 1);
-    currentIndex -= 1;
-  })
-  next.addEventListener('click', () => {
-    setSlide(currentIndex + 1);
-    currentIndex += 1;
-  })
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+   }
+
+function showSlides(n) {
+  if (n > slides.length) {slideIndex = 1};
+  if (n < 1) {slideIndex = slides.length};
+  for (i = 0; i < slides.length; i++) {
+    dots[i].className = dots[i].className.replace("dot-indicator_active", "");
+    captions[i].className = captions[i].className.replace(" caption-text_active", "");}
+  img.style.backgroundImage = `url(${slides[slideIndex-1].image})`;
+  city.innerText = slides[slideIndex-1].city;
+  area.innerText = slides[slideIndex-1].area;
+  time.innerText = slides[slideIndex-1].time;
+  dots[slideIndex-1].className += " dot-indicator_active";
+  captions[slideIndex-1].className += " caption-text_active";
+}
