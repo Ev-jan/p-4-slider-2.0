1b554c6288b26b8e44c7037692677e6ec88f3111
Ev-jan <evgen.mazyar@gmail.com>
2022-11-09 15:44:46 +0400

    finilized w/o animation

--- js/script-2.js
+++ js/script-2.js
@@ -16,30 +16,32 @@ const slides = [
     }
   ]
   
-  let city = document.querySelector(".info-block__text_city");
-  let area = document.querySelector(".info-block__text_area");
-  let time = document.querySelector(".info-block__text_time");
-  let img = document.querySelector(".slideshow-container");
-  let dots = document.getElementsByClassName("dot-indicator");
-  let captions = document.getElementsByClassName("caption-text");
-  let slideIndex = 1;
+let city = document.querySelector(".info-block__text_city");
+let area = document.querySelector(".info-block__text_area");
+let time = document.querySelector(".info-block__text_time");
+let img = document.querySelector(".slideshow-container");
+let dots = document.getElementsByClassName("dot-indicator");
+let captions = document.getElementsByClassName("caption-text");
+let slideIndex = 1;
   
-  showSlides(slideIndex);
+showSlides(slideIndex);
 
-  function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n) {
+showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
   showSlides(slideIndex = n);
-   }
+}
 
 function showSlides(n) {
   if (n > slides.length) {slideIndex = 1};
   if (n < 1) {slideIndex = slides.length};
   for (i = 0; i < slides.length; i++) {
     dots[i].className = dots[i].className.replace("dot-indicator_active", "");
-    captions[i].className = captions[i].className.replace(" caption-text_active", "");}
+    captions[i].className = captions[i].className.replace(" caption-text_active", "");
+  }
+  
   img.style.backgroundImage = `url(${slides[slideIndex-1].image})`;
   city.innerText = slides[slideIndex-1].city;
   area.innerText = slides[slideIndex-1].area;
